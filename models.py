361b5e0991f7e102b8a033fdf0a386c702a324a2
dmutka <daemon.mutka98@gmail.com>
2024-03-14 16:23:47 -0500

    Added models.py to prevent circular imports from main

---
+++ models.py
@@ -0,0 +1,9 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+DATABASE_URI = os.environ.get('DATABASE_URI')
+engine = create_engine(DATABASE_URI, echo=True)
+Base = declarative_base()
