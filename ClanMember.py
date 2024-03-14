6fee51849cec8b233b6e36a2dc062527b956b62c
dmutka <daemon.mutka98@gmail.com>
2024-03-14 15:23:31 -0500

    Added columns to ClanMember class/table

--- ClanMember.py
+++ ClanMember.py
@@ -1,6 +1,10 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, SmallInteger
 from main import Base
 
 
 class ClanMember(Base):
-    __tablename__ = "ClanMember"
+    __tablename__ = "ClanMembers"
+
+    id = Column(String, primary_key=True)
+    position = Column(String, nullable=False)
+    ranking = Column(SmallInteger)
