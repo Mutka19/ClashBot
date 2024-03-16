5af00b49a02628b85130808ad27739001659b989
dmutka <daemon.mutka98@gmail.com>
2024-03-15 20:01:47 -0500

    Updated help n=message

--- controller/clash_client.py
+++ controller/clash_client.py
@@ -11,26 +11,26 @@ class ClashClient:
     def __init__(self):
         # Get api key from env and set base url
         load_dotenv(find_dotenv())
-        self.base_url = "https://api.clashofclans.com/v1"
-        self.api_key = os.getenv("CLASH_API_KEY")
+        self.__base_url = "https://api.clashofclans.com/v1"
+        self.__api_key = os.getenv("CLASH_API_KEY")
 
         # Setup database handler
-        self.db = DatabaseHandler()
+        self.__db = DatabaseHandler()
 
         # Set clan name to null
-        self.clan_tag = None
+        self.__clan_tag = None
 
     def __del__(self):
         # Close db session when clash client instance is destroyed
-        self.db.close()
+        self.__db.close()
 
     def set_clan(self, clan_tag: str) -> None:
-        self.clan_tag = clan_tag
+        self.__clan_tag = clan_tag
 
     def fetch_clan_info(self, clan_tag: str) -> dict:
         # Set url and headers for api call
-        url = f"{self.base_url}/clans/%23{clan_tag}"
-        headers = {"Authorization": f"Bearer {self.api_key}"}
+        url = f"{self.__base_url}/clans/%23{clan_tag}"
+        headers = {"Authorization": f"Bearer {self.__api_key}"}
 
         # Make call and get response
         response = requests.get(url, headers=headers)
@@ -40,8 +40,8 @@ class ClashClient:
 
     def fetch_clan_members(self, clan_tag: str) -> dict:
         # Set url and headers for api call
-        url = f"{self.base_url}/clans/%23{clan_tag}/members"
-        headers = {"Authorization": f"Bearer {self.api_key}"}
+        url = f"{self.__base_url}/clans/%23{clan_tag}/members"
+        headers = {"Authorization": f"Bearer {self.__api_key}"}
 
         # Make call and get response
         response = requests.get(url, headers=headers)
@@ -51,15 +51,15 @@ class ClashClient:
 
     def fetch_my_members(self) -> dict:
         # Check if clan_tag has been set, if not return empty list
-        if self.clan_tag is not None:
-            return self.fetch_clan_info(self.clan_tag)
+        if self.__clan_tag is not None:
+            return self.fetch_clan_info(self.__clan_tag)
         else:
             return {}
 
     def fetch_clan_warlog(self, clan_tag: str) -> dict:
         # Set url and headers for api call
-        url = f"{self.base_url}/clans/%23{clan_tag}/warlog"
-        headers = {"Authorization": f"Bearer {self.api_key}"}
+        url = f"{self.__base_url}/clans/%23{clan_tag}/warlog"
+        headers = {"Authorization": f"Bearer {self.__api_key}"}
 
         # Make call and get response
         response = requests.get(url, headers=headers)
@@ -69,15 +69,15 @@ class ClashClient:
 
     def fetch_my_warlog(self) -> dict:
         # Check if clan_tag has been set, if not return empty list
-        if self.clan_tag is not None:
-            return self.fetch_clan_warlog(self.clan_tag)
+        if self.__clan_tag is not None:
+            return self.fetch_clan_warlog(self.__clan_tag)
         else:
             return {}
 
     def fetch_current_clan_war(self, clan_tag: str) -> dict:
         # Set url and headers for api call
-        url = f"{self.base_url}/clans/%23{clan_tag}/currentwar"
-        headers = {"Authorization": f"Bearer {self.api_key}"}
+        url = f"{self.__base_url}/clans/%23{clan_tag}/currentwar"
+        headers = {"Authorization": f"Bearer {self.__api_key}"}
 
         # Make call and get response
         response = requests.get(url, headers=headers)
@@ -87,23 +87,23 @@ class ClashClient:
 
     def fetch_my_current_clan_war(self) -> dict:
         # Check if clan_tag has been set, if not return empty list
-        if self.clan_tag is not None:
-            return self.fetch_current_clan_war(self.clan_tag)
+        if self.__clan_tag is not None:
+            return self.fetch_current_clan_war(self.__clan_tag)
         else:
             return {}
 
     # Records all clan members to database
     def record_clan_members(self) -> None:
         # Return immediately if no clan tag has been set
-        if self.clan_tag is None:
+        if self.__clan_tag is None:
             return
 
         # Use fetch clan members to return list of all clan members and their stats
-        clan_members = self.fetch_clan_members(self.clan_tag)["items"]
+        clan_members = self.fetch_clan_members(self.__clan_tag)["items"]
 
         # For each clan member add a clanmember object in database
         for member in clan_members:
-            self.db.add_object(
+            self.__db.add_object(
                 ClanMember(
                     id=member["tag"],
                     name=member["name"],
@@ -113,16 +113,16 @@ class ClashClient:
             )
 
         # Commit clan member objects to database
-        self.db.commit()
+        self.__db.commit()
 
     # Record percentage of attacks player has used in wars
     def record_player_clan_war_stats(self) -> None:
         # Return immediately if no clan tag has been set
-        if self.clan_tag is None:
+        if self.__clan_tag is None:
             return
 
         # Fetch current clan war stats
-        war = self.fetch_current_clan_war(self.clan_tag)
+        war = self.fetch_current_clan_war(self.__clan_tag)
 
         # For each member in clan war
         for member in war["clan"]["members"]:
@@ -139,7 +139,7 @@ class ClashClient:
                 # Make an attack record for each attack used in war
                 for attack in member["attacks"]:
                     defender = war["opponent"]["members"][attack["defenderTag"]]
-                    self.db.session.add_object(
+                    self.__db.session.add_object(
                         ClanWarAttackRecord(
                             stars=attack["stars"],
                             percentage=attack["destructionPercentage"],
@@ -154,7 +154,7 @@ class ClashClient:
 
             # If no attacks were made in war create an according clan war player record
             else:
-                self.db.session.add_object(
+                self.__db.session.add_object(
                     ClanWarPlayerRecord(
                         attacks_used=0,
                         stars=0,
@@ -165,28 +165,28 @@ class ClashClient:
                 )
 
             # Commit all records to database
-            self.db.session.commit()
+            self.__db.session.commit()
 
     def get_all_members(self) -> list:
-        return self.db.query(ClanMember).all()
+        return self.__db.query(ClanMember).all()
 
     def get_player_records(self, member_id: str) -> list:
         return (
-            self.db.query(ClanWarPlayerRecord)
+            self.__db.query(ClanWarPlayerRecord)
             .filter(ClanWarPlayerRecord.member_id == member_id)
             .all()
         )
 
     def get_attack_records(self, cw_player_record_id: str) -> list:
         return (
-            self.db.query(ClanWarAttackRecord)
+            self.__db.query(ClanWarAttackRecord)
             .filter(ClanWarAttackRecord.cw_player_record_id == cw_player_record_id)
             .all()
         )
 
     def update_efficiency(self, member_id: str) -> None:
         # Query clan member by id
-        member = self.db.query(ClanMember).filter_by(id=member_id).first()
+        member = self.__db.query(ClanMember).filter_by(id=member_id).first()
 
         # If the member is not found return
         if member is None:
@@ -241,4 +241,4 @@ class ClashClient:
         member.efficiency = min(100.00, efficiency)
 
         # Commit changes in member participation to database
-        self.db.commit()
+        self.__db.commit()
