4184f30cbe5804e72b97a983ac00990e1fdc4621
dmutka <daemon.mutka98@gmail.com>
2024-03-20 12:23:04 -0500

    Moved setup method

--- controller/responses.py
+++ controller/responses.py
@@ -31,6 +31,18 @@ class Responses:
                     message[1] = message[1][1:]
                 return ", ".join([member["name"] for member in self.clash_client.fetch_clan_members(message[1])])
 
+        if message[0] == "/setup" and len(message) == 2:
+            # If hashtag on clan tag remove it and continue
+            if message[1][0] == "#":
+                message[1] = message[1][1:]
+
+            # If clan cannot be reached by api
+            if "reason" in self.clash_client.fetch_clan_info(message[1]):
+                return "Clan not found."
+            else:
+                self.clash_client.set_clan(message[1])
+                return "Clan found and set."
+
         if message[0] == "/war" and message[1] == "update":
             # Check for any extra unwanted arguments
             if len(message) > 2:
